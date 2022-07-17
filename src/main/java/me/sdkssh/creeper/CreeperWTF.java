01d559de0025f4d23c24c325a3d24a1c3cae10e9
SDKSSH <sdk_ssh@protonmail.com>
2022-07-17 13:20:49 +0200

    Register listener to main class

--- src/main/java/me/sdkssh/creeper/CreeperWTF.java
+++ src/main/java/me/sdkssh/creeper/CreeperWTF.java
@@ -1,7 +1,11 @@
 package me.sdkssh.creeper;
 
+import me.sdkssh.creeper.events.CreeperListeners;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CreeperWTF extends JavaPlugin {
-
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new CreeperListeners(), this);
+    }
 }
