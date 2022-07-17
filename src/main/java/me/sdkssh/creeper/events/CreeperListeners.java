77e72e7d04840cf2e151440473fd6baf2fdc8166
SDKSSH <sdk_ssh@protonmail.com>
2022-07-17 13:14:48 +0200

    Init entity event

--- src/main/java/me/sdkssh/creeper/events/CreeperListeners.java
+++ src/main/java/me/sdkssh/creeper/events/CreeperListeners.java
@@ -1,6 +1,12 @@
 package me.sdkssh.creeper.events;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class CreeperListeners implements Listener {
+    @EventHandler
+    public void onMobSpawn(EntitySpawnEvent e){
+
+    }
 }
