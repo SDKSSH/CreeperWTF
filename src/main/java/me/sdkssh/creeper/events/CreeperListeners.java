7e24af8a7b88aa3263484c26216978d40bd1fd3f
SDKSSH <sdk_ssh@protonmail.com>
2022-07-17 13:20:06 +0200

    Finish CreeperListeners

--- src/main/java/me/sdkssh/creeper/events/CreeperListeners.java
+++ src/main/java/me/sdkssh/creeper/events/CreeperListeners.java
@@ -1,15 +1,27 @@
 package me.sdkssh.creeper.events;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class CreeperListeners implements Listener {
     @EventHandler
     public void onMobSpawn(EntitySpawnEvent e){
         Entity ent = e.getEntity();
         if(ent.getType() != EntityType.CREEPER) return;
+        LivingEntity c = (LivingEntity) ent;
+        c.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 999999, 255, false));
+        c.getEquipment().setHelmet(new ItemStack(Material.TNT));
+        c.getEquipment().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+        c.getEquipment().setHelmetDropChance(0);
+        c.getEquipment().setLeggingsDropChance(0);
     }
 }
