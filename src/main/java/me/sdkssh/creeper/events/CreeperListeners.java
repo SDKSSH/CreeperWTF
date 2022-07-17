e43faad38031736fd5c85d25c9b55feb5af710d0
SDKSSH <sdk_ssh@protonmail.com>
2022-07-17 13:15:40 +0200

    Return events when it's not creeper

--- src/main/java/me/sdkssh/creeper/events/CreeperListeners.java
+++ src/main/java/me/sdkssh/creeper/events/CreeperListeners.java
@@ -1,5 +1,7 @@
 package me.sdkssh.creeper.events;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -7,6 +9,7 @@ import org.bukkit.event.entity.EntitySpawnEvent;
 public class CreeperListeners implements Listener {
     @EventHandler
     public void onMobSpawn(EntitySpawnEvent e){
-
+        Entity ent = e.getEntity();
+        if(ent.getType() != EntityType.CREEPER) return;
     }
 }
